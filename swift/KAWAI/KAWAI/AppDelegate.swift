d92c7fb3f0a292dd30578a7596618862608ed069
ToshikiIkeda <g2117004@fun.ac.jp>
2018-10-20 19:04:02 +0900

    Revert "Check motion"

--- swift/KAWAI/KAWAI/AppDelegate.swift
+++ swift/KAWAI/KAWAI/AppDelegate.swift
@@ -10,8 +10,9 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
     var window: UIWindow?
-    var originImage:UIImage?
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
